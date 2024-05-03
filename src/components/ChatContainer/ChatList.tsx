23aedd22041593f6d8a24cf5c00433c5ff76bb15
yziori <ms6rx7861@gmail.com>
2024-05-03 15:59:02 +0900

    ✨ feat: ChatListコンポーネントのUI作成

--- src/components/ChatContainer/ChatList.tsx
+++ src/components/ChatContainer/ChatList.tsx
@@ -1,7 +1,22 @@
 const ChatList = () => {
 	return (
 		<div className="w-64 bg-gray-800">
-			{/* チャットリストをここに挿入する */}
+			<div className="px-4 py-3 mb-4 border-b border-gray-700">
+				<span className="font-bold text-gray-300">チャンネル</span>
+			</div>
+			<div className="overflow-y-auto">
+				<div className="px-4 py-1 hover:bg-gray-700">
+					<div className="px-4 py-1 text-gray-300 hover:text-white">
+						# Random
+					</div>
+				</div>
+			</div>
+			<div className="px-4 py-2">
+				{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+				<button className="text-gray-300 hover:text-white">
+					+ チャンネルを追加する
+				</button>
+			</div>
 		</div>
 	);
 };

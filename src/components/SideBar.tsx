7060e6d8c59bf71a1333ddf4ca23c7241e0e9f21
yziori <ms6rx7861@gmail.com>
2024-05-03 15:44:43 +0900

    ✨ feat: サイドバーのUI作成

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,28 @@
+import { ChatBubble, Home } from "@mui/icons-material";
+
 const SideBar = () => {
-	// biome-ignore lint/style/useSelfClosingElements: <explanation>
-	return <div className="w-16 h-screen bg-gray-900"></div>;
+	return (
+		<div className="w-16 h-screen bg-gray-900 flex flex-col items-center text-white">
+			<div className="py-5 flex flex-col items-center">
+				<div className="bg-gray-700 p-2 rounded-lg">
+					<Home />
+				</div>
+				<span className="text-xs">Home</span>
+			</div>
+			<div className="py-5 flex flex-col items-center">
+				<div className="bg-gray-700 p-2 rounded-lg">
+					<ChatBubble />
+				</div>
+				<span className="text-xs">DM</span>
+			</div>
+			<div className="py-5 mt-auto mx-2 flex flex-col items-center">
+				<div className="bg-gray-700 p-2 rounded-lg">
+					<img src="/default-user-icon.webp" alt="" />
+				</div>
+				<span className="text-xs">DM</span>
+			</div>
+		</div>
+	);
 };
 
 export default SideBar;

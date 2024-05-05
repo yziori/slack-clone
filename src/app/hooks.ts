b31c699d1075ced1400247be59d96787eee0bf0b
yziori <ms6rx7861@gmail.com>
2024-05-05 23:59:47 +0900

    🚧 work:  useDispathとuseSelectorに型を付けてラップ

---
+++ src/app/hooks.ts
@@ -0,0 +1,9 @@
+import {
+	type TypedUseSelectorHook,
+	useDispatch,
+	useSelector,
+} from "react-redux";
+import type { AppDispatch, RootState } from "./store";
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

49e271cb27a50cb78d2b9b7462364ca61f1a0391
yziori <ms6rx7861@gmail.com>
2024-05-06 00:01:04 +0900

    🚧 work: アプリ全体にストアを提供

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
 	ReactDOM.createRoot(rootElement).render(
 		<React.StrictMode>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</React.StrictMode>,
 	);
 } else {

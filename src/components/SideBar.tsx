9c3b3bc0af74145bc526be46e73d9ed98047b96c
yziori <ms6rx7861@gmail.com>
2024-05-08 02:38:25 +0900

    ✨ feat: ユーザー取得API繋ぎこみ

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,23 @@
 import { ChatBubble, Home } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { useAppSelector } from "../app/hooks";
+import { getUser } from "../features/user/userAPI";
+import type { User } from "../types/User";
 
 const SideBar = () => {
+	const userId = useAppSelector((state) => state.user.userId);
+	const [user, setUser] = useState<User | null>();
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			if (userId) {
+				const userRef = await getUser(userId);
+				setUser(userRef);
+			}
+		};
+		fetchUser();
+	}, [userId]);
+
 	return (
 		<div className="w-16 h-screen bg-gray-900 flex flex-col items-center text-white">
 			<div className="py-5 flex flex-col items-center">
@@ -19,7 +36,7 @@ const SideBar = () => {
 				<div className="bg-gray-700 p-2 rounded-lg">
 					<img src="/default-user-icon.png" alt="" />
 				</div>
-				<span className="text-xs">DM</span>
+				<span className="text-xs">{user?.displayName}</span>
 			</div>
 		</div>
 	);

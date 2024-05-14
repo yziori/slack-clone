71fd14914887c97c125296da461e400cdba768fb
yziori <ms6rx7861@gmail.com>
2024-05-15 02:42:07 +0900

    ✨ feat: チャンネル取得API繋ぎこみ

--- src/components/ChatContainer/ChatList.tsx
+++ src/components/ChatContainer/ChatList.tsx
@@ -1,19 +1,30 @@
+import { useEffect, useState } from "react";
+import { subscribeChannels } from "../../features/channel/channelAPI";
+import type { ChannelRef } from "../../types/Channel";
+import ChannelCell from "./ChannelCell";
+
 const ChatList = () => {
+	const [channelRefs, setChannelRefs] = useState<ChannelRef[]>([]);
+
+	useEffect(() => {
+		const unsubscribe = subscribeChannels((channelRefs) => {
+			setChannelRefs(channelRefs);
+		});
+		return () => unsubscribe();
+	}, []);
+
 	return (
 		<div className="w-64 bg-gray-800">
 			<div className="px-4 py-3 mb-4 border-b border-gray-700">
 				<span className="font-bold text-gray-300">チャンネル</span>
 			</div>
 			<div className="overflow-y-auto">
-				<div className="px-4 py-1 hover:bg-gray-700">
-					<div className="px-4 py-1 text-gray-300 hover:text-white">
-						# Random
-					</div>
-				</div>
+				{channelRefs.map(({ channel, id }) => (
+					<ChannelCell channel={channel} id={id} key={id} />
+				))}
 			</div>
 			<div className="px-4 py-2">
-				{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
-				<button className="text-gray-300 hover:text-white">
+				<button type="button" className="text-gray-300 hover:text-white">
 					+ チャンネルを追加する
 				</button>
 			</div>

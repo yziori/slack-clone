1482b6d46b62a55cb034a4bda28cf7859b7a6b0c
yziori <ms6rx7861@gmail.com>
2024-05-15 03:26:50 +0900

    ✨ feat: チャンネル追加API実装

--- src/features/channel/channelAPI.ts
+++ src/features/channel/channelAPI.ts
@@ -1,4 +1,6 @@
 import {
+	Timestamp,
+	addDoc,
 	collection,
 	getFirestore,
 	onSnapshot,
@@ -9,6 +11,19 @@ import type { Channel, ChannelRef } from "../../types/Channel";
 
 const db = getFirestore(firebaseApp);
 
+/**
+ * Firestoreの`channels`コレクションを購読し、変更がある度に指定されたコールバック関数を通じて
+ * チャンネルのリスト更新する。この関数は、Firestoreのスナップショットリスナーを設定し、
+ * データの更新があるたびに、`onChannelUpdated`関数を呼び出す。
+ * @param onChannelsUpdated
+ * @returns {Function} Firebase のリスナーを解除するためのアンサブスクライブ関数。
+ * @example
+ * const unsubscribe = subscribeChannels((channels) => {
+ *   console.log('Received channels:', channels);
+ * });
+ * // リスナーを解除するには
+ * unsubscribe();
+ */
 export const subscribeChannels = (
 	onChannelsUpdated: (channels: ChannelRef[]) => void,
 ) => {
@@ -31,4 +46,26 @@ export const subscribeChannels = (
 			console.error("Failed to subscribe channels:", error);
 		},
 	);
+};
+
+/**
+ * チャンネルを追加する関数。
+ * 公式ドキュメント：https://firebase.google.com/docs/firestore/manage-data/add-data?hl=ja#add_a_document
+ * @param channel - 追加するチャンネルデータ
+ */
+export const postChannel = async (channel: Channel) => {
+	await addDoc(collection(db, "channels"), channel);
+};
+
+/**
+ * チャンネルデータを作成する関数
+ * @param name チャンネル名
+ * @returns {Object} チャンネル名と作成時刻のオブジェクトを返す
+ */
+export const createChannel = (name: string): Channel => {
+	const timestamp = Timestamp.fromDate(new Date());
+	return {
+		name: name,
+		createAt: timestamp,
+	};
 };

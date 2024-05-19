d6659954b7e964ff749da667d4ac1c16ac164c81
yziori <ms6rx7861@gmail.com>
2024-05-19 17:24:34 +0900

    ✨ feat: メッセージ関連のAPI繋ぎこみ

--- src/components/ChatContainer/MessageArea.tsx
+++ src/components/ChatContainer/MessageArea.tsx
@@ -1,34 +1,60 @@
 import { TextareaAutosize } from "@mui/base/TextareaAutosize";
 import SendIcon from "@mui/icons-material/Send";
+import { type ChangeEvent, type KeyboardEvent, useState } from "react";
+import { useAppSelector } from "../../app/hooks";
+import { createMessage } from "../../features/message/messageAPI";
+import type { MessageRef } from "../../types/Message";
+import MessageTile from "./MessageTile";
 
 const MessageArea = () => {
+	const [messageRefs, setMessageRefs] = useState<MessageRef[]>();
+	const userId = useAppSelector((state) => state.user.userId);
+	const channelId = useAppSelector((state) => state.channel.currentChannelId);
+
+	const [message, setMessage] = useState<string>("");
+	const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+		setMessage(e.target.value);
+	};
+
+	const sendMessage = async () => {
+		if (userId) {
+			try {
+				await postMessage(createMessage(userId, channelId, message));
+				setMessage("");
+			} catch (error) {
+				console.error("Error Sending Message: ", error);
+			}
+		}
+	};
+
+	const handleKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+		if ((e.metaKey || e.ctrlKey) && e.code === "Enter") {
+			sendMessage();
+		}
+	};
 	return (
 		<div className="flex-1 flex-col flex bg-gray-500">
 			<div className="p-4 m-3">
-				<div className="bg-gray-700 p-3 m-3 rounded-lg">
-					<div className="flex items-center mb-2">
-						<img
-							src="/default-user-icon.png"
-							alt=""
-							className="w-10 h-10 rounded-full mr-2"
-						/>
-						<div>
-							<div className="text-sm text-white font-semibold">unknown</div>
-							<div className="text-sm text-gray-400">2014/01/01 00:00:00</div>
-						</div>
-					</div>
-					<p className="text-gray-300">やあ</p>
-				</div>
+				{messageRefs?.map((messageRef) => (
+					<MessageTile message={messageRef.message} key={messageRef.id} />
+				))}
 			</div>
 
 			<div className="mt-auto px-4 py-2 bottom-0 bg-gray-900">
 				<div className="flex items-center">
 					<TextareaAutosize
-						placeholder="# randomへメッセージ"
+						placeholder="メッセージを入力"
 						className="flex-1 bg-gray-700 text-white p-2 mx-2 rounded-lg focus:outline-none"
+						onChange={handleInputChange}
+						onKeyDown={handleKeyDown}
+						value={message}
 					/>
-					{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
-					<button className="text-gray-400 hover:text-white">
+
+					<button
+						type="button"
+						className="text-gray-400 hover:text-white"
+						onClick={sendMessage}
+					>
 						<SendIcon />
 					</button>
 				</div>
---
+++ src/components/ChatContainer/MessageTile.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { getUser } from "../../features/user/userAPI";
+import type { Message } from "../../types/Message";
+import type { User } from "../../types/User";
+
+interface MessageTileProps {
+	message: Message;
+}
+
+const MessageTile = (message: MessageTileProps) => {
+	const [owner, setOwnner] = useState<User | null>(null);
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+				const ownerData = await getUser(message.message.userId);
+				if (ownerData) {
+					setOwnner(ownerData);
+				}
+			} catch (error) {
+				setOwnner(null);
+			}
+		};
+		fetchUser();
+	}, []);
+
+	return (
+		<div className="bg-gray-700 p-3 m-3 rounded-lg">
+			<div className="flex items-center mb-2">
+				<img
+					src={owner?.profilePicture || "/default-user-icon.png"}
+					alt="プロフィール画像"
+					className="w-10 h-10 rounded-full mr-2"
+				/>
+				<div>
+					<div className="text-sm text-white font-semibold">
+						{owner?.displayName || "unkown"}
+					</div>
+					<div className="text-sm text-gray-400">
+						{message.message.createAt.toDate.toLocaleString() || ""}
+					</div>
+				</div>
+			</div>
+			<p className="text-gray-300">{message.message.text}</p>
+		</div>
+	);
+};
+
+export default MessageTile;
